8e6d6c13fd48665da7d3dc36b55f7ecc0453c5ee
Malcolm Langfield <35980963+langfield@users.noreply.github.com>
2022-06-21 12:15:55 -0400

    Complete very bad, bad proof.

--- Mergesort.lean
+++ Mergesort.lean
@@ -1,8 +1,6 @@
--- import Mathlib
+import Mathlib
 
-notation "ℕ" => Nat
-
-def merge : List ℕ → List ℕ → List ℕ
+def merge : List Nat → List Nat → List Nat
 | xs, [] => xs
 | [], ys => ys
 | (x :: xs), (y :: ys) => if x < y then x :: (merge xs ( y :: ys))
@@ -15,48 +13,22 @@ def first_half {α : Type} (xs : List α) : List α :=
 def second_half {α : Type} (xs : List α) : List α :=
   List.drop (xs.length / 2) xs
 
-theorem min_le_left (a b : ℕ) : Nat.min a b ≤ a := by
-  simp [Nat.min]
-  by_cases h : a ≤ b
-  simp
-  split
-  simp
-  simp
-  split
-  simp
-  rw [Nat.not_le_eq] at h
-  apply @Nat.le_trans b (b + 1) a
-  simp
-  exact Nat.le_succ b
-  exact h
+def half_add_one_lt_add_two (n : Nat) : n / 2 + 1 < n + 2 := by
+  induction n with
+  | zero => simp
+  | succ m h =>
+    rw [Nat.succ_eq_add_one]
+    have zero_lt_m_add_one : 0 < m + 1 := by
+      apply Nat.zero_lt_succ
+    have one_lt_two : 1 < 2 := by simp
+    have div_le_self := @Nat.div_lt_self (m + 1) 2 zero_lt_m_add_one one_lt_two
+    apply Nat.succ_le_succ
+    apply Nat.succ_le_succ
+    exact Nat.le_of_lt div_le_self
 
-theorem min_eq_left (h : a ≤ b) : Nat.min a b = a := by simp [Nat.min, h]
-theorem zero_min (a : ℕ) : Nat.min 0 a = 0 := min_eq_left (Nat.zero_le a)
+def one_add_one_eq_two : 1 + 1 = 2 := by simp
 
-theorem min_eq_right (h : b ≤ a) : Nat.min a b = b := by rw [Nat.min_comm a b]; exact min_eq_left h
-theorem min_zero (a : ℕ) : min a 0 = 0 := min_eq_right (zero_le a)
-
-@[simp] theorem length_take : ∀ (i : Nat) (l : List α), List.length (List.take i l) = Nat.min i (List.length l)
-| 0, l => by
-  simp [zero_min]
-  rw [List.take]
-  rw [List.length]
-| Nat.succ n, [] => by simp [Nat.min_zero]
-| Nat.succ n, a :: l => by simp [Nat.min_succ_succ, add_one, length_take]
-
--- theorem length_take_le (n) (l : List α) : length (take n l) ≤ n := by simp [min_le_left]
-theorem length_take_le : List.length (List.take n l) ≤ n := by
-  sorry
-  
-  
-  
-
-theorem nonempty_first_half_lt {x : ℕ} {xs : List ℕ} : List.length (first_half (x :: xs)) < List.length (x :: xs) := by
-  rw [first_half]
-  simp
-  sorry
-
-def mergesort : List ℕ → List ℕ
+def mergesort : List Nat → List Nat
 | [] => []
 | [a] => [a]
 | (x :: (y :: ys)) => 
@@ -66,20 +38,44 @@ def mergesort : List ℕ → List ℕ
     rw [Nat.succ_eq_add_one]
     rw [Nat.add_assoc]
     simp
-    let lem : 1 + 1 = 2 := by simp
-    rw [lem]
+    rw [one_add_one_eq_two]
     rw [first_half]
     rw [List.length_cons]
     rw [List.length_cons]
     simp
     rw [Nat.succ_eq_add_one]
     rw [Nat.add_assoc]
-    rw [lem]
+    rw [one_add_one_eq_two]
     simp
-    sorry
+    rw [Nat.succ_eq_add_one]
+    simp
+    have half_lemma := half_add_one_lt_add_two (List.length ys)
+    rw [min_eq_left]
+    simp
+    exact half_lemma
+    simp
+    exact Nat.le_of_lt half_lemma
   have : List.length (second_half (x :: (y :: ys))) < List.length (x :: (y :: ys)) := by
-    rw [List.length_cons]
-    sorry
+    rw [List.length_cons, List.length_cons]
+    simp
+    rw [Nat.succ_eq_add_one]
+    simp
+    rw [Nat.add_assoc]
+    simp
+    rw [one_add_one_eq_two]
+    simp
+    rw [second_half]
+    simp
+    rw [Nat.succ_eq_add_one]
+    simp
+    rw [Nat.add_assoc, one_add_one_eq_two]
+    simp
+    rw [Nat.succ_eq_add_one]
+    apply Nat.sub_lt_self
+    rw [← Nat.succ_eq_add_one]
+    exact Nat.zero_lt_succ (List.length ys / 2)
+    apply Nat.le_of_lt
+    apply half_add_one_lt_add_two
   merge (mergesort <| first_half (x :: (y :: ys))) (mergesort <| second_half (x :: (y :: ys)))
 termination_by mergesort xs => xs.length
 
