710fea5116d32104f7f41adcf5aa31ffea77fcca
Malcolm Langfield <35980963+langfield@users.noreply.github.com>
2022-06-24 09:44:12 -0400

    Got stuck on `unfold`.

--- Quicksort.lean
+++ Quicksort.lean
@@ -2,22 +2,26 @@ import Mathlib
 
 #check List.filterAux
 
-def filterAux (p : α → Bool) : List α → List α → List α
-  | [],    rs => rs.reverse
-  | a::as, rs => match p a with
-     | true  => filterAux p as (a::rs)
-     | false => filterAux p as rs
-
-@[inline] def filter (p : α → Bool) (as : List α) : List α :=
-  filterAux p as []
-
 def quick2 : List ℤ → List ℤ
 | [] => []
 | List.cons x xs =>
-  have first_half_terminates : List.length (filter (fun y => decide (y < x)) xs) < Nat.succ (List.length xs) := by
-    rw [filter]
+  have first_half_terminates : List.length (List.filter (fun y => decide (y < x)) xs) < Nat.succ (List.length xs) := by
+    rw [List.filter]
+    cases xs
+    · rw [List.filterAux]
+      rw [List.reverse] 
+      rw [List.reverseAux]
+      rw [List.length]
+      rw [Nat.succ_eq_add_one, Nat.zero_add]
+      simp
+    · unfold List.filterAux
+      rename_i head tail
+      simp_arith
+      split
+
+
+      
+  have first_half_terminates : List.length (List.filter (fun y => decide (y ≥ x)) xs) < Nat.succ (List.length xs) :=
     sorry
-  have first_half_terminates : List.length (filter (fun y => decide (y ≥ x)) xs) < Nat.succ (List.length xs) :=
-    sorry
-  quick2 (filter (λ y => y < x) xs) ++ x :: (quick2 (filter (λ y => y ≥ x) xs))
+  quick2 (List.filter (λ y => y < x) xs) ++ x :: (quick2 (List.filter (λ y => y ≥ x) xs))
 termination_by quick2 xs => xs.length
